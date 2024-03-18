e81125d61fab6921d79817c7da9514d2fd9ce045
JuanDPomares <160240478+JuanDPomares@users.noreply.github.com>
2024-03-18 08:28:20 -0500

    Tarea de actividad 2 ciclos 18/03/24

---
+++ Vuelo.py
@@ -0,0 +1,17 @@
+Diccionario_de_vuelo = {
+    'Aereolinia:':'Avianca',
+    'Vuelo:':'AV3102',
+    'Origen:':"CTG",
+    'Destino:':'MDE',
+    'Tipo_de_maleta:':['Cabina', 'Mano', 'Bodega'],
+   
+}
+for key in Diccionario_de_vuelo:
+    print(key)
+
+for key, value in Diccionario_de_vuelo.items():
+    print(key, value) 
+    
+
+
+        
