1a6d11f78b62c2b5874c1f3efe9cf4414d085200
JuanDPomares <160240478+JuanDPomares@users.noreply.github.com>
2024-04-03 21:03:47 -0500

    Update2 Vectores_java.java

--- Vectores_java/src/main/java/com/mycompany/vectores_java/Vectores_java.java
+++ Vectores_java/src/main/java/com/mycompany/vectores_java/Vectores_java.java
@@ -26,45 +26,47 @@ public class Vectores_java {
         listaMasDe5.add(5);
         listaMasDe5.add(6);
             
-            listaMasDe5.add(7);
-
-        int longitudListaVacia = listaVacia.size();
-         int longitudListaMasDe5 = listaMasDe5.size();
-
-        int primerElementoListaMasDe5 = listaMasDe5.get(0);
-         int elementoCentralListaMasDe5 = listaMasDe5.get(listaMasDe5.size() / 2);
+        listaMasDe5.add(7);
+        System.out.println(listaMasDe5);
         
+        int longitudListaVacia = listaVacia.size();
+        int longitudListaMasDe5 = listaMasDe5.size();
+        int primerElementoListaMasDe5 = listaMasDe5.get(0);
+        int elementoCentralListaMasDe5 = listaMasDe5.get(listaMasDe5.size() / 2);
         int ultimoElementoListaMasDe5 = listaMasDe5.get(listaMasDe5.size() - 1);
-
+            System.out.println("la longitud de la lista mas de 5 es de:" +longitudListaMasDe5);
+            System.out.println("la longitud de la lista vacia de:" +longitudListaVacia);
+        
+        
         ArrayList<Object> datosPersonales = new ArrayList<>();
-                datosPersonales.add("Juan David Julio Pomares");
-       
+            datosPersonales.add("Juan David Julio Pomares");
             datosPersonales.add(19);
-       
             datosPersonales.add(1.73);
             datosPersonales.add("soltero");
-       
-        datosPersonales.add("cartagena");
-
+            datosPersonales.add("cartagena");
+            System.out.println(datosPersonales);
         
       
         ArrayList<String> itCompanies = new ArrayList<>();
-         itCompanies.add("Facebook");
+        itCompanies.add("Facebook");
         itCompanies.add("Google");
         
         itCompanies.add("Microsoft");
         itCompanies.add("Apple");
         
-            itCompanies.add("IBM");
+        itCompanies.add("IBM");
         itCompanies.add("Oracle");
         itCompanies.add("Amazon");
+        System.out.println(itCompanies);
+            
+        itCompanies.add(2, "Twitter");
         
-            itCompanies.add(2, "Twitter");
-        
-            String empresaABuscar = "Google";
+            
+        String empresaABuscar = "Google";
        
-        boolean existeEmpresa = itCompanies.contains(empresaABuscar);
-
+        boolean existeEmpresa = itCompanies.contains("Google");
+        System.out.println("¿Existe google en la empresa? "  +existeEmpresa);
+        
         Collections.sort(itCompanies);
 
         
