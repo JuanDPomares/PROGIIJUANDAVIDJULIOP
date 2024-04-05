56907baf4c3ffd64184579e96fe4bc8341938bed
JuanDPomares <160240478+JuanDPomares@users.noreply.github.com>
2024-04-05 08:37:16 -0500

    Add files via upload

---
+++ diccionario python.py
@@ -0,0 +1,28 @@
+perro = {}
+perro['Nombre'] = 'pablo'
+perro ['color'] = 'café'
+perro ['raza'] = 'pitbul'
+perro ['patas'] = '4 patas'
+perro ['edad'] = '6 años'
+print(perro)
+
+estudiante = {}
+estudiante ['Nombre'] = 'Juan', 'David'
+estudiante [ 'apellido'] = 'Julio', 'pomares'
+estudiante ['Sexo'] = 'Masculino'
+estudiante ['edad'] = '19'
+estudiante ['estado civil'] = 'soltero'
+estudiante ['habilidades'] = 'disciplinado', 'adaptable', 'dinamico'
+estudiante ['pais'] = 'Colombia'
+estudiante ['Ciudad'] = 'cartagena'
+estudiante ['habilidades'] = 'capacitado'
+
+del perro
+
+print (estudiante)
+print(len(estudiante))
+values= estudiante.values()
+print(values)
+keys = estudiante.keys()
+print(keys)
+print(estudiante.items())
